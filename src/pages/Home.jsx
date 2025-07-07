a5f8ecf23283b174178905835deee8a591c86862
mohdjunaid0612 <mohdjunaid0612@gmail.com>
2025-07-07 23:59:49 +0530

    add some section in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import SkillsService from "../component/SkillsService";
 import careerPath from "../assets/images/img2.jpg";
 import ExperienceTimeline from "../component/ExperienceTimeline";
 import HomeProjectSlider from "../component/HomeProjectSlider";
+import bgImg from "../assets/images/bg/body-bg.jpg";
 
 const Home = () => {
   return (
@@ -226,6 +227,99 @@ const Home = () => {
           </p>
           <div className="mt-10">
             <HomeProjectSlider />
+          </div>
+        </div>
+      </section>
+      <section className="py-20">
+        <div className="container mx-auto md:px-4 px-3">
+          <div className="flex justify-center">
+            <div className="w-[60%]">
+              <p className="text-sm uppercase font-semibold text-center">
+                <i className="fab fa-asterisk me-2 text-primary"></i> Working
+                Process
+              </p>
+              <h3 className="text-5xl leading-tight text-center mt-5">
+                Our seamless{" "}
+                <span className="text-primary font-extrabold">process for</span>{" "}
+                exceptional results
+              </h3>
+            </div>
+          </div>
+          <div className="mt-16 grid grid-cols-1 md:grid-cols-4 gap-6">
+            <div className="border hover:-translate-y-2 ease-in-out border-gray-200 p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-transform duration-300">
+              <div className="bg-violet-600 w-[60px] h-[60px] rounded-xl text-2xl flex justify-center items-center font-semibold text-white">
+                01
+              </div>
+              <h4 className="text-2xl text-zinc my-5 font-semibold">
+                Discovery & Direction
+              </h4>
+              <p className="text-gray-600 mt-3">
+                I dive deep into your vision, goals, and target users. Whether
+                it’s a startup idea or an enterprise overhaul, I start by asking
+                the right questions to align with your mission.
+              </p>
+            </div>
+            <div className="border hover:-translate-y-2 ease-in-out border-gray-200 p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-transform duration-300">
+              <div className="bg-yellow-400 w-[60px] h-[60px] rounded-xl text-2xl flex justify-center items-center font-semibold text-white">
+                02
+              </div>
+              <h4 className="text-2xl text-zinc my-5 font-semibold">
+                Design-Driven Planning
+              </h4>
+              <p className="text-gray-600 mt-3">
+                Wireframes, style guides, and components—we plan it all before a
+                single line of code. I translate your ideas into structured
+                UI/UX blueprints.
+              </p>
+            </div>
+            <div className="border hover:-translate-y-2 ease-in-out border-gray-200 p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-transform duration-300">
+              <div className="bg-green-500 w-[60px] h-[60px] rounded-xl text-2xl flex justify-center items-center font-semibold text-white">
+                03
+              </div>
+              <h4 className="text-2xl text-zinc my-5 font-semibold">
+                Frontend Engineering
+              </h4>
+              <p className="text-gray-600 mt-3">
+                This is where the magic happens. I build responsive, accessible,
+                lightning-fast interfaces using HTML5, SCSS, JS, and frameworks
+                like Bootstrap, Alpine.js, and React.
+              </p>
+            </div>
+            <div className="border hover:-translate-y-2 ease-in-out border-gray-200 p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-transform duration-300">
+              <div className="bg-red-500 w-[60px] h-[60px] rounded-xl text-2xl flex justify-center items-center font-semibold text-white">
+                04
+              </div>
+              <h4 className="text-2xl text-zinc my-5 font-semibold">
+                Launch & Elevate
+              </h4>
+              <p className="text-gray-600 mt-3">
+                Once it’s live, I don’t ghost. I optimize for performance, SEO,
+                and accessibility—and stay on board to iterate, scale, and keep
+                your product fresh.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="py-20 bg-gray-100">
+        <div className="container mx-auto  md:px-4 px-3">
+          <div className="relative">
+            <h3 className="text-xxl text-center text-gray-500 text-center font-semibold">
+              LET'S COLLABORATE
+            </h3>
+            <h4 className="text-9xl text-zinc-700 mt-5 text-center font-bold">
+              LET'S WORK
+            </h4>
+            <h4 className="text-9xl text-zinc-700 mt-3 text-center font-bold">
+              TOGETHER
+            </h4>
+            <a
+              href="#"
+              className="inline-block flex items-center flex-col justify-center absolute w-[150px] h-[150px] rounded-full bg-primary top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 hover:scale-110 ease-in-out duration-300"
+            >
+              <i className="fa fa-arrow-right d-block text-zinc-900 -rotate-45 text-xl"></i>
+              <p className="font-semibold text-xl">Get in Touch</p>
+            </a>
           </div>
         </div>
       </section>
